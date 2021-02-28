2f776701e672a43d6af37271050be5794da545b6
Cloudy-dv <michal.motak95@gmail.com>
2021-02-28 20:28:18 +0100

    add migrations, max length isb changed to 100

--- lib/models.py
+++ lib/models.py
@@ -5,7 +5,7 @@ class Books(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     publication_date = models.DateField(null=True)
-    isbn = models.CharField(max_length=13, unique=True)
+    isbn = models.CharField(max_length=100, unique=True)
     pages = models.IntegerField(default=0)
     art_cover = models.URLField(max_length=100)
     published_lang = models.CharField(max_length=20)
