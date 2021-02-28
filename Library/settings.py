fc298374fab237881702d82b27648b40866d1504
Cloudy-dv <michal.motak95@gmail.com>
2021-02-28 17:53:32 +0100

    first commit

--- Library/settings.py
+++ Library/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'nb+b=a*w@)h$^j-n(*!7q#mc8b%%i1p^a0#rfl(n0uu&km5h&m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
